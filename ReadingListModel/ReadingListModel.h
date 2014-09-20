26e5004eab757281ee08d4884239eb3ed1a19cce
Jonathan Lehr <jonathan@aboutobjects.com>
2014-09-19 20:53:58 -0400

    Minor cleanup.

--- ReadingListModel/ReadingListModel.h
+++ ReadingListModel/ReadingListModel.h
@@ -2,13 +2,7 @@
 // Copyright (C) 2014 About Objects, Inc. All Rights Reserved.
 // See LICENSE.txt for this example's licensing information.
 //
-
 #import <Foundation/Foundation.h>
 
-//! Project version number for Things.
 FOUNDATION_EXPORT double ReadingListModelVersionNumber;
-
-//! Project version string for Things.
 FOUNDATION_EXPORT const unsigned char ReadingListModelVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Things/PublicHeader.h>
